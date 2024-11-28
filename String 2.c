6a4b76c1c37c26ae0d75562c570d27879a9c0c7f
Navitha <n7865925@gmail.com>
2024-11-29 04:47:02 +0530

    Delete String 2.c

--- String 2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char str[] = "Hello";
-    int length = strlen(str);
-    printf("Length of the string: %d\n", length);
-    return 0;
-}
